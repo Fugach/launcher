2bfc74dce9ef1c26f6e02c0acb7e2ec0e3110473
Fugach <savakrivonosov44@gmail.com>
2026-05-02 12:59:26 +0500

    + Искалка версий (release, beta, alpha...)

--- clear.bat
+++ clear.bat
@@ -1,3 +1,4 @@
 del portablemc.exe
 rmdir /s /q  "mc_main"
 rmdir /s /q "bin"
+rmdir /s /q "instances"
--- main.py
+++ main.py
@@ -1,32 +1,41 @@
-import os, subprocess, shlex, sys, wget, zipfile, shutil
-from traceback import extract_tb
-
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtQml import QQmlApplicationEngine
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QApplication, QLabel
+import os, subprocess, wget, zipfile
 
 def portablemc(command: str):
-    args = shlex.split(command)
+    args = command.split()
     return subprocess.run(
-        [sys.executable, "-m", "portablemc", *args],
+        [os.path.join("bin", "portablemc.exe"), *args],
         capture_output=True,
         text=True,
         encoding="utf-8"
     )
 def install():
-    if not os.path.isfile("portablemc.exe"):
+    if not os.path.exists("bin/portablemc.exe"):
         print("Downloading portablemc...")
         wget.download(
             "https://github.com/theorzr/portablemc/releases/download/v5.0.3/portablemc-5.0.3-windows-x86_64-msvc.zip",\
             "portablemc-5.0.3-windows-x86_64-msvc.zip")
         print("\nDone. Unzipping...")
-        with zipfile.ZipFile("portablemc-5.0.3-windows-x86_64-msvc.zip", "r") as zip:
+        with zipfile.ZipFile("portablemc-5.0.3-windows-x86_64-msvc.zip") as zip:
             zip.extract("portablemc.exe")
         os.remove("portablemc-5.0.3-windows-x86_64-msvc.zip")
+        if not os.path.isdir("bin"):
+            os.makedirs("bin")
+        os.replace("portablemc.exe", "bin/portablemc.exe")
         print("Now portablemc is here!")
     else:
         print("portablemc is already here!")
-    if not os.path.isdir("bin"):
-        os.makedirs("bin")
     if not os.path.isdir("mc_main"):
         os.makedirs("mc_main")
-
+    if not os.path.isdir("instances"):
+        os.makedirs("instances")
+def get_versions(channel):
+    output = list(portablemc("search --channel " + channel + " --output machine").stdout.split())
+    output = [item for item in output if not any(x in item for x in ["+00:00", "name", "channel", "release_date", "row", "sep", channel])]
+    return output
 if __name__ == "__main__":
     install()
+    get_versions("alpha")
