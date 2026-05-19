6b2aacc7f12a450bae052407f25b7a6d15c5e86e
Fugach <savakrivonosov44@gmail.com>
2026-05-19 21:03:22 +0500

    + Опциональный шаг со скачиванием portablemc при первом запуске P.S. Похоже, что я понимаю pyside6! *\(^-^)/*

--- assets/scripts/first_start.py
+++ assets/scripts/first_start.py
@@ -16,15 +16,30 @@ class MainWindow(QMainWindow):
         layout_pmc.setAlignment(Qt.AlignmentFlag.AlignCenter)
         layout_acc = QVBoxLayout()
 
-
+        title = QLabel("Приветствую вас в NAME!")
+        title_font = title.font()
+        title_font.setPointSize(32)
+        title.setFont(title_font)
+        title.setAlignment(
+            Qt.AlignmentFlag.AlignTop
+            | Qt.AlignmentFlag.AlignHCenter
+        )
+        layout_pmc.addWidget(title)
+        main_text = QLabel("Прежде чем начать работу, нужно настроить лаунчер под вас.")
+        main_text_font = main_text.font()
+        main_text_font.setPointSize(16)
+        main_text.setFont(main_text_font)
+        main_text.setWordWrap(True)
+        main_text.setAlignment(
+            Qt.AlignmentFlag.AlignTop
+            | Qt.AlignmentFlag.AlignHCenter
+        )
+        layout_pmc.addWidget(main_text)
         self.button = QPushButton("Погнали")
         self.button.clicked.connect(self.download_pmc)
         self.button.pressed.connect(lambda : self.button.setText("и..."))
-        self.button.released.connect(lambda :  self.button.setText(
-            random.choice(["Эй! Может начнём?", "Зачем отпустил? :(", "Нажми уже!!!!!!", "ОШИБКА: КНОПКА НЕ НАЖАТА"]))
-            if self.button.isEnabled() else None)
         self.button.setFixedSize(QSize(200, 80))
-        layout_pmc.addWidget(self.button)
+        layout_pmc.addWidget(self.button, alignment=Qt.AlignmentFlag.AlignCenter)
 
 
         page_pmc = QWidget()
@@ -40,18 +55,64 @@ class MainWindow(QMainWindow):
         widget.setLayout(self.stacked_layout)
         self.setCentralWidget(widget)
     def download_pmc(self):
-        self.button.setText("работаем...")
-        install()
-        if os.path.exists("bin/portablemc.exe"):
-            self.button.setText("Готово!")
-            self.button.setDisabled(True)
+        self.button.setDisabled(True)
+        if not os.path.exists("bin/portablemc.exe"):
+            dlg = PMC_dialog()
+            if dlg.exec():
+                self.button.setText("Скачиваем portablemc...")
+                self.button.setDisabled(True)
+                QApplication.processEvents()
+                install()
+            else:
+                self.button.clicked.disconnect(self.download_pmc)
+                self.button.clicked.connect(lambda: self.stacked_layout.setCurrentIndex(1))
+                QApplication.processEvents()
+                self.button.setText("Дальше")
+                self.button.setDisabled(False)
+                return None
+        else:
+            self.button.setText("Portablemc уже тут, отлично!")
+            self.button.clicked.disconnect(self.download_pmc)
             QApplication.processEvents()
             time.sleep(1)
             self.stacked_layout.setCurrentIndex(1)
+            return None
+        if os.path.exists("bin/portablemc.exe"):
+            self.button.setDisabled(True)
+            self.button.setText("Готово!")
+            self.button.clicked.disconnect(self.download_pmc)
+            self.button.clicked.connect(lambda : self.stacked_layout.setCurrentIndex(1))
+            QApplication.processEvents()
+            time.sleep(1)
+            self.button.setText("Дальше")
+            self.button.setDisabled(False)
         else:
             self.button.setText("Что-то навернулось... Может стоит попробовать ещё раз?")
             self.button.setDisabled(False)
 
+
+class PMC_dialog(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Уведомление")
+        buttons = (
+            QDialogButtonBox.StandardButton.Ok
+            | QDialogButtonBox.StandardButton.Abort
+        )
+        self.buttonbox = QDialogButtonBox(buttons)
+        self.buttonbox.accepted.connect(self.accept)
+        self.buttonbox.rejected.connect(self.reject)
+
+        self.layout = QVBoxLayout()
+        message = QLabel("В папке с лаунчером не обнаружен portablemc, скачать его сейчас?\n"
+                         "Он обязателен для запуска майнкрафта.\n"
+                         "Скачивание произойдёт на фоне без открытия вашего браузера.\n"
+                         "В данный момент этот шаг полностью опционален.\n"
+                         "Вы сможете выполнить его после начальной настройки.")
+        self.layout.addWidget(message)
+        self.layout.addWidget(self.buttonbox)
+        self.setLayout(self.layout)
 def start():
     app = QApplication(sys.argv)
 
@@ -63,17 +124,17 @@ def start():
 # def download():
 #     install()
 
-def login():
-    # запускаем логин в лицензионный акк на фоне
-    def worker():
-        raw_output = portablemc("auth login --output machine", True, False)
-    # raw_output = threading.Thread(target=lambda : portablemc("auth login --output machine", True)).start()
-    threading.Thread(target=worker, daemon=True).start()
-
-def get_link_and_code(inp):
-    print(inp)
-    if "auth_device_code" in str(inp):
-        print(str(inp).split())
+# def login():
+#     # запускаем логин в лицензионный акк на фоне
+#     def worker():
+#         raw_output = portablemc("auth login --output machine", True, False)
+#     # raw_output = threading.Thread(target=lambda : portablemc("auth login --output machine", True)).start()
+#     threading.Thread(target=worker, daemon=True).start()
+#
+# def get_link_and_code(inp):
+#     print(inp)
+#     if "auth_device_code" in str(inp):
+#         print(str(inp).split())
 
 
 
